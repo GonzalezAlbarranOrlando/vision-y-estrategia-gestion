736331dcdf1471f195dbcf47900d42bf2b53f454
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2022-11-01 12:49:41 -0600

    nosotros v1

--- src/components/pages/Nosotros.vue
+++ src/components/pages/Nosotros.vue
@@ -5,13 +5,172 @@
       <p class="animate__animated animate__slideInDown">Nosotros</p>
     </div>
     <!--Animated title end-->
+    <div class="row mb-5 mt-1">
+      <div class="col-lg-6 py-2">
+        <!--card MISIÓN-->
+        <div class="card">
+          <div class="card-header card-header-nosotros">
+            <h5 class="card-title">MISIÓN</h5>
+          </div>
+          <div class="card-body card-body-nosotros">
+            <p class="card-text">
+              Somos una Organización que ofrece servicios profesionales con
+              personal altamente competente, que promueve entre nuestros
+              clientes y partes interesadas un ambiente de confianza; con
+              precios competitivos, equitativos y atractivos conforme a la
+              dinámica del mercado, tenemos amplia capacidad para dar
+              certidumbre en la ejecución de nuestras evaluaciones; entregamos
+              oportunamente los informes de evaluación a las instituciones.
+            </p>
+          </div>
+        </div>
+        <!--card MISIÓN end-->
+      </div>
+      <div class="col-lg-6 py-2">
+        <!--card VISIÓN-->
+        <div class="card">
+          <div class="card-header card-header-nosotros">
+            <h5 class="card-title">VISIÓN</h5>
+          </div>
+          <div class="card-body card-body-nosotros">
+            <p class="card-text">
+              Ser en el mercado de la evaluación un referente distintivo por
+              nuestras capacidades innovadoras que nos definirá como una marca
+              exitosa en la evaluación, tendremos la mejor oferta en cuanto al
+              desarrollo de evaluaciones; seremos un equipo de especialistas con
+              capacidad para trabajar con altos estándares de calidad, armonía,
+              espíritu de compañerismo y trabajo en equipo, lo que nos
+              convertirá en el equipo evaluador de mayor presencia nacional.
+            </p>
+          </div>
+        </div>
+        <!--card VISIÓN end-->
+      </div>
+    </div>
+
+    <h2 class="my-h2">EQUIPO EVALUADOR</h2>
+    <div class="row justify-content-center" id="equipo">
+      <div class="col-sm-6 col-lg-3 py-3">
+        <div class="card">
+          <div class="card-body">
+            <img
+              src="@/assets/img/equipo/01-VICENTE.jpg"
+              class="w-100 rounded-circle"
+              alt="..."
+            />
+          </div>
+          <div class="card-header card-header-nosotros">
+            <b>Vicente Martínez Iniesta</b><br />subtitle
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3 py-3">
+        <div class="card">
+          <div class="card-body">
+            <img
+              src="@/assets/img/equipo/02-LALO.jpg"
+              class="w-100 rounded-circle"
+              alt="..."
+            />
+          </div>
+          <div class="card-header card-header-nosotros">
+            <b>José Eduardo Jerónimo Herrera</b><br />subtitle
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3 py-3">
+        <div class="card">
+          <div class="card-body">
+            <img
+              src="@/assets/img/equipo/03-MIRIAM.jpg"
+              class="w-100 rounded-circle"
+              alt="..."
+            />
+          </div>
+          <div class="card-header card-header-nosotros">
+            <b>Miriam Ruíz Martínez</b><br />subtitle
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3 py-3">
+        <div class="card">
+          <div class="card-body">
+            <img
+              src="@/assets/img/equipo/04-ANDREA.jpg"
+              class="w-100 rounded-circle"
+              alt="..."
+            />
+          </div>
+          <div class="card-header card-header-nosotros">
+            <b>Andrea González Palma</b><br />subtitle
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3 py-3">
+        <div class="card">
+          <div class="card-body">
+            <img
+              src="@/assets/img/equipo/05-HAROLD.jpg"
+              class="w-100 rounded-circle"
+              alt="..."
+            />
+          </div>
+          <div class="card-header card-header-nosotros">
+            <b>Harold Alexander Carrillo Cuenca</b><br />subtitle
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3 py-3">
+        <div class="card">
+          <div class="card-body">
+            <img
+              src="@/assets/img/equipo/06-CARLOS.jpg"
+              class="w-100 rounded-circle"
+              alt="..."
+            />
+          </div>
+          <div class="card-header card-header-nosotros">
+            <b>Carlos Beltrán Jaramillo</b><br />subtitle
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3 py-3">
+        <div class="card">
+          <div class="card-body">
+            <img
+              src="@/assets/img/equipo/07-FELIPE.jpg"
+              class="w-100 rounded-circle"
+              alt="..."
+            />
+          </div>
+          <div class="card-header card-header-nosotros">
+            <b>Felipe Gabino Macedo</b><br />subtitle
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3 py-3">
+        <div class="card">
+          <div class="card-body">
+            <img
+              src="@/assets/img/equipo/08-RAMON.jpg"
+              class="w-100 rounded-circle"
+              alt="..."
+            />
+          </div>
+          <div class="card-header card-header-nosotros">
+            <b>Ramón Cuevas Martínez</b><br />subtitle
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+  
 <script>
 export default {
   name: "NosotrosPage",
 };
 </script>
-
+  
 <style>
 </style>
