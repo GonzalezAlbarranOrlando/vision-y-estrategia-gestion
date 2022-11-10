0d61ea0a7c80e9e32b6b873b3f2fc7e5df1a81c2
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2022-11-10 14:22:45 -0600

    email js functionality

--- src/components/pages/Contacto.vue
+++ src/components/pages/Contacto.vue
@@ -4,18 +4,27 @@
     <div class="animate-div">
       <p class="animate__animated animate__slideInDown">Contáctanos</p>
     </div>
-    <!--Animated title end-->
     <!-- Form Code -->
-    <form @submit.prevent="myValidation" class="my-5">
+    <form @submit.prevent="validationCheck" class="my-5">
       <div class="row">
         <div class="col-md-6 mb-3">
           <label>Nombre(s):*</label>
-          <input type="text" class="form-control" v-model="firstName" />
+          <input
+            type="text"
+            class="form-control"
+            v-model="firstName"
+            name="firstName"
+          />
           <div v-if="firstNameError" class="error">{{ firstNameError }}</div>
         </div>
         <div class="col-md-6 mb-3">
           <label>Apellidos:*</label>
-          <input type="text" class="form-control" v-model="lastName" />
+          <input
+            type="text"
+            class="form-control"
+            v-model="lastName"
+            name="lastName"
+          />
           <div v-if="lastNameError" class="error">{{ lastNameError }}</div>
         </div>
       </div>
@@ -27,12 +36,18 @@
             class="form-control"
             placeholder="name@example.com"
             v-model="email"
+            name="email"
           />
           <div v-if="emailError" class="error">{{ emailError }}</div>
         </div>
         <div class="col-md-6 mb-3">
           <label>Teléfono:*</label>
-          <input type="tel" class="form-control" v-model="phoneNumber" />
+          <input
+            type="tel"
+            class="form-control"
+            v-model="phoneNumber"
+            name="phoneNumber"
+          />
           <div v-if="phoneNumberError" class="error">
             {{ phoneNumberError }}
           </div>
@@ -40,8 +55,14 @@
       </div>
       <div class="mb-3">
         <label class="form-label">Mensaje:*</label>
-        <textarea class="form-control" rows="3" v-model="message"></textarea>
+        <textarea
+          class="form-control"
+          rows="3"
+          v-model="message"
+          name="message"
+        ></textarea>
       </div>
+      <input type="hidden" v-model="pageName" name="pageName" />
       <div class="obligatorio">* Campos obligatorios</div>
       <div style="text-align: center">
         <button class="btn btn-secondary" type="submit">Enviar</button>
@@ -52,6 +73,7 @@
 </template>
 
 <script>
+import emailjs from "emailjs-com";
 export default {
   name: "ContactoPage",
   data() {
@@ -61,6 +83,7 @@ export default {
       email: "",
       phoneNumber: "",
       message: "",
+      pageName: "Gestión",
       // Errors
       firstNameError: "",
       lastNameError: "",
@@ -70,6 +93,40 @@ export default {
     };
   },
   methods: {
+    validationCheck(e) {
+      if (this.myValidation()) {
+        this.sendEmail(e);
+        alert("Correo enviado");
+      }
+    },
+    sendEmail(e) {
+      try {
+        emailjs.sendForm(
+          "service_exwol9f",
+          "template_l7rtv6i",
+          e.target,
+          "HOK9n0XHI6-GN66lP",
+          {
+            firstName: this.firstName,
+            lastName: this.lastName,
+            email: this.email,
+            phoneNumber: this.phoneNumber,
+            message: this.message,
+            pageName: this.pageName,
+          }
+        );
+        console.log("Correo Enviado");
+      } catch (error) {
+        console.log("Error:");
+        console.log({ error });
+      }
+      // Reset form field
+      this.firstName = "";
+      this.lastName = "";
+      this.email = "";
+      this.phoneNumber = "";
+      this.message = "";
+    },
     myValidation() {
       // required validation
       if (
@@ -82,7 +139,8 @@ export default {
         )
       ) {
         alert("Todos los campos son obligatorios.");
-        return;
+        console.log("Todos los campos son obligatorios.");
+        return false;
       }
       //First/Last name regular expression validator
       //Orlando, orlando, ORLANDO, orlando glez, Ana Sofia, Orlando González, McMiller, John D'Largy, John Doe-Smith
@@ -116,19 +174,13 @@ export default {
           this.phoneNumberError ||
           this.messageError
         )
-      )
-        alert(
-          "TODOS LOS DATOS SON CORRECTOS\nNombre(s): " +
-            this.firstName +
-            "\nApellido(s): " +
-            this.lastName +
-            "\nCorreo: " +
-            this.email +
-            "\nNúmero: " +
-            this.phoneNumber +
-            "\nMensaje: " +
-            this.message
-        );
+      ) {
+        console.log("Validación correcta");
+        return true;
+      } else {
+        console.log("Validación incorrecta");
+        return false;
+      }
     },
   },
 };
